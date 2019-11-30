e19e4fcfd3686ba28bc703f556df8a347eb84022
rayfill <rayfill@gmail.com>
2019-11-30 21:46:32 +0900

    fix version

--- pixiv_fanbox_downloader.user.js
+++ pixiv_fanbox_downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pixiv fanbox resource saver
 // @namespace    https://pixiv.fanbox.net/
-// @version      20191130
+// @version      20191130.1
 // @description  pixiv fanbox article downloader
 // @downloadURL  https://raw.githubusercontent.com/rayfill/userscripts/master/pixiv_fanbox_downloader.user.js
 // @updateURL    https://raw.githubusercontent.com/rayfill/userscripts/master/pixiv_fanbox_downloader.user.js
