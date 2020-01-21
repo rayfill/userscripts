3cc688f1472af0f08777a736b76ee7d042a8ce2e
rayfill <rayfill@gmail.com>
2020-01-22 06:59:41 +0900

    add support 'text' mode

--- pixiv_fanbox_downloader.user.js
+++ pixiv_fanbox_downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pixiv fanbox resource saver
 // @namespace    https://pixiv.fanbox.net/
-// @version      20191214
+// @version      20200122
 // @description  pixiv fanbox article downloader
 // @downloadURL  https://raw.githubusercontent.com/rayfill/userscripts/master/pixiv_fanbox_downloader.user.js
 // @updateURL    https://raw.githubusercontent.com/rayfill/userscripts/master/pixiv_fanbox_downloader.user.js
@@ -27,271 +27,287 @@ const creatorURL = new RegExp("^https://www.pixiv.net/ajax/fanbox/creator[?]user
 const itemMap = new Map();
 
 function collectItems(items) {
-  for (let item of items) {
-    let id = item.id;
-    itemMap.set(id, item);
-  }
+    for (let item of items) {
+        let id = item.id;
+        itemMap.set(id, item);
+    }
 
-  console.log(Array.from(itemMap.keys()));
+    console.log(Array.from(itemMap.keys()));
 }
 
 function main() {
-  xhrHook2((url, resType, content) => {
+    xhrHook2((url, resType, content) => {
 
-    console.log("url:", url);
-    let match = null;
-    let json = null;
-    if (match = indexURL.exec(url)) {
-      if (resType !== "json") {
-        json = JSON.parse(content);
-      } else {
-        json = content;
-      }
-      let items = json.body.postListForHome.items.concat(json.body.postListOfSupporting.items);
-      collectItems(items);
+        console.log("url:", url);
+        let match = null;
+        let json = null;
+        if (match = indexURL.exec(url)) {
+            if (resType !== "json") {
+                json = JSON.parse(content);
+            } else {
+                json = content;
+            }
+            let items = json.body.postListForHome.items.concat(json.body.postListOfSupporting.items);
+            collectItems(items);
 
-    } else if (match = postListHomeURL.exec(url)) {
-      if (resType !== "json") {
-        json = JSON.parse(content);
-      } else {
-        json = content;
-      }
-      let items = json.body.items;
-      collectItems(items);
+        } else if (match = postListHomeURL.exec(url)) {
+            if (resType !== "json") {
+                json = JSON.parse(content);
+            } else {
+                json = content;
+            }
+            let items = json.body.items;
+            collectItems(items);
 
-    } else if (match = postURL.exec(url)) {
-      if (resType !== "json") {
-        json = JSON.parse(content);
-      } else {
-        json = content;
-      }
-      let items = [json.body];
-      collectItems(items);
+        } else if (match = postURL.exec(url)) {
+            if (resType !== "json") {
+                json = JSON.parse(content);
+            } else {
+                json = content;
+            }
+            let items = [json.body];
+            collectItems(items);
 
-    } else if (match = creatorURL.exec(url)) {
-      if (resType !== "json") {
-        json = JSON.parse(content);
-      } else {
-        json = content;
-      }
-      let items = json.body.post.items;
-      collectItems(items);
-    }
-  });
+        } else if (match = creatorURL.exec(url)) {
+            if (resType !== "json") {
+                json = JSON.parse(content);
+            } else {
+                json = content;
+            }
+            let items = json.body.post.items;
+            collectItems(items);
+        }
+    });
 
-  let observer = new MutationObserver((records, observer) => {
-    window.postMessage({ type: "mutation" }, "*");
-  });
+    let observer = new MutationObserver((records, observer) => {
+        window.postMessage({ type: "mutation" }, "*");
+    });
 
-  window.addEventListener("message", mutationHandler);
+    window.addEventListener("message", mutationHandler);
 
-  observer.observe(document, { childList: true, subtree: true });
+    observer.observe(document, { childList: true, subtree: true });
 }
 
 const postIdPattern = new RegExp('^https://www.pixiv.net/fanbox/creator/([0-9]+)/post/([0-9]+)$');
 function getArticleId(article) {
-  let url = new URL(window.location.href);
-  url.search = "";
-  let match = postIdPattern.exec(url.toString());
-  if (match === null)
-    throw new TypeError("invalid url");
+    let url = new URL(window.location.href);
+    url.search = "";
+    let match = postIdPattern.exec(url.toString());
+    if (match === null)
+        throw new TypeError("invalid url");
 
-  return match[2];
+    return match[2];
 
 }
 
 function handleImage(body) {
-  return body.images.map((elm, idx) => {
-    return {
-      filename: `${idx}.${elm.extension}`,
-      url: elm.originalUrl
-    }
-  });
+    return body.images.map((elm, idx) => {
+        return {
+            filename: `${idx}.${elm.extension}`,
+            url: elm.originalUrl
+        }
+    });
 }
 
 function handleFile(body) {
-  return body.files.map((elm, idx) => {
-    return {
-      filename: `${elm.name}.${elm.extension}`,
-      url: elm.url
-    }
-  });
+    return body.files.map((elm, idx) => {
+        return {
+            filename: `${elm.name}.${elm.extension}`,
+            url: elm.url
+        }
+    });
+}
+
+function handleText(body) {
+    return [];
 }
 
 function handleArticle(body) {
-  console.log("body", body);
-  let result = [];
-  for (let imageId in body.imageMap) {
-    let { id: id, extension: extension, originalUrl: originalUrl } = body.imageMap[imageId];
-    result.push({
-      filename: `${id}.${extension}`,
-      url: originalUrl
-    });
-  }
-  return result;
+    console.log("body", body);
+    let result = [];
+    for (let imageId in body.imageMap) {
+        let { id: id, extension: extension, originalUrl: originalUrl } = body.imageMap[imageId];
+        result.push({
+            filename: `${id}.${extension}`,
+            url: originalUrl
+        });
+    }
+    return result;
 }
 
 function makeProgressHandler() {
-  let loaded = 0;
-  return (evt) => {
-    let currentLoaded = evt.loaded;
-    let amount = currentLoaded - loaded;
-    loaded = currentLoaded;
-    window.postMessage({ type: "progress", amount: amount }, "*");
-  };
-}
-    
-function fetchResources(resources) {
-  return resources.map((elm) => {
-    return {
-      filename: elm.filename,
-      blob: GM_fetch(elm.url, { onprogress: makeProgressHandler()}).then((res) => {
-        if (!res.ok)
-          throw new TypeError("resource fetch failed", res);
-        return res.blob();
-      })
+    let loaded = 0;
+    return (evt) => {
+        let currentLoaded = evt.loaded;
+        let amount = currentLoaded - loaded;
+        loaded = currentLoaded;
+        window.postMessage({ type: "progress", amount: amount }, "*");
     };
-  });
+}
+
+function fetchResources(resources) {
+    return resources.map((elm) => {
+        return {
+            filename: elm.filename,
+            blob: GM_fetch(elm.url, { onprogress: makeProgressHandler()}).then((res) => {
+                if (!res.ok)
+                    throw new TypeError("resource fetch failed", res);
+                return res.blob();
+            })
+        };
+    });
+}
+
+function parseText(body) {
+    return body.text;
 }
 
 function parseArticle(body) {
-  let imageMap = body.imageMap;
-  let blocks = body.blocks.map((block) => {
-    switch (block.type) {
-    case "p":
-      return `<p>${block.text}</p>`;
-      break;
-      
-    case "image":
-      return `<img src="article/${block.imageId}.${imageMap[block.imageId].extension}"></img>`;
-      break;
-      
-    default:
-      alert("unknown block type:", block.type);
-      debugger;
-    }
-  });
+    let imageMap = body.imageMap;
+    let blocks = body.blocks.map((block) => {
+        switch (block.type) {
+            case "p":
+                return `<p>${block.text}</p>`;
+                break;
 
-  return '<!DOCTYPE html><head><meta charset="UTF-8"/></head><body>' + blocks.join('') + "</body></html>";
+            case "image":
+                return `<img src="article/${block.imageId}.${imageMap[block.imageId].extension}"></img>`;
+                break;
+
+            default:
+                alert("unknown block type:", block.type);
+                debugger;
+        }
+    });
+
+    return '<!DOCTYPE html><head><meta charset="UTF-8"/></head><body>' + blocks.join('') + "</body></html>";
 }
 
 function download(id, btn) {
-  let info = itemMap.get(id);
-  console.log("info", info);
+    let info = itemMap.get(id);
+    console.log("info", info);
 
-  let {
-    title: title,
-    coverImageUrl: cover,
-    updateDateTime: lastModified,
-    type: type,
-    body: body,
-    user: user
-  } = info;
-  let text = body.text;
-  console.log({ title: title, cover: cover, lastModified: lastModified, type: type, body: body, user: user });
+    let {
+        title: title,
+        coverImageUrl: cover,
+        updateDateTime: lastModified,
+        type: type,
+        body: body,
+        user: user
+    } = info;
+    let text = body.text;
+    console.log({ title: title, cover: cover, lastModified: lastModified, type: type, body: body, user: user });
 
-  let resources = [];
-  switch (type) {
-  case "image":
-    resources = handleImage(body);
-    break;
+    let resources = [];
+    switch (type) {
+        case "image":
+            resources = handleImage(body);
+            break;
 
-  case "file":
-    resources = handleFile(body);
-    break;
+        case "file":
+            resources = handleFile(body);
+            break;
 
-  case "article":
-    resources = handleArticle(body);
-    break;
+        case "text":
+            resources = handleText(body);
 
-  default:
-    throw new TypeError("unhandled type: " + type);
-  }
+        case "article":
+            resources = handleArticle(body);
+            break;
 
-  console.log("resources:", resources);
-  let res = fetchResources(resources);
-  let zip = new JSZip();
-  if (type === "article") {
-    let html = parseArticle(body);
-    zip.file("message.html", html);
-  } else {
-    zip.file("message.txt", text);
-  }
-  zip.file("cover.jpg", GM_fetch(cover).then((res) => {
-    if (!res.ok)
-      throw new TypeError("cover fetch failed:", res);
-    return res.blob();
-  }));
-  res.forEach((elm) => {
-    zip.file(type + "/" + elm.filename, elm.blob);
-  });
-  zip.generateAsync({ type: "blob" }, (metadata) => {
-    if (metadata.currentFile) {
-      window.postMessage({
-	type: "compress",
-	filename: metadata.currentFile,
-	percent: Math.round(metadata.percent * 100) / 100
-      }, "*");
+        default:
+            throw new TypeError("unhandled type: " + type);
     }
-  }).then((blob) => {
-    saveAs(blob, title + ".zip");
-    localStorage.setItem(id, true);
-  });
+
+    console.log("resources:", resources);
+    let res = fetchResources(resources);
+    let zip = new JSZip();
+    if (type === "article") {
+        let html = parseArticle(body);
+        zip.file("message.html", html);
+    } else if (type === "text") {
+        let text = parseText(body);
+        zip.file("message.txt", text);
+    } else {
+        zip.file("message.txt", text);
+    }
+    if (cover != null) {
+        zip.file("cover.jpg", GM_fetch(cover).then((res) => {
+            if (!res.ok)
+                throw new TypeError("cover fetch failed:", res);
+            return res.blob();
+        }));
+    }
+    res.forEach((elm) => {
+        zip.file(type + "/" + elm.filename, elm.blob);
+    });
+    zip.generateAsync({ type: "blob" }, (metadata) => {
+        if (metadata.currentFile) {
+            window.postMessage({
+                type: "compress",
+                filename: metadata.currentFile,
+                percent: Math.round(metadata.percent * 100) / 100
+            }, "*");
+        }
+    }).then((blob) => {
+        saveAs(blob, title + ".zip");
+        localStorage.setItem(id, true);
+    });
 }
 
 const proceedColor = "rgb(0, 150, 250)";
 const unproceedColor = "rgb(180, 180, 180)";
 function mutationHandler(evt) {
-  let { type: type } = evt.data;
-  if (type !== "mutation") {
-    return;
-  }
-  console.log("mutation");
-
-  let article = document.querySelector('article');
-  if (article !== null) {
-    if (article.dataset.proceed) {
-      return;
+    let { type: type } = evt.data;
+    if (type !== "mutation") {
+        return;
     }
+    console.log("mutation");
 
-    let id = getArticleId(article);
-    if (!itemMap.get(id)) {
-      return;
+    let article = document.querySelector('article');
+    if (article !== null) {
+        if (article.dataset.proceed) {
+            return;
+        }
+
+        let id = getArticleId(article);
+        if (!itemMap.get(id)) {
+            return;
+        }
+
+        let button = document.createElement('button');
+        button.style.backgroundColor = localStorage.getItem(id) ? proceedColor : unproceedColor;
+        button.addEventListener('click', (evt) => {
+            download(id, button);
+        });
+        button.innerText = "click to save";
+
+        progressReceiver(button);
+
+        article.appendChild(button);
+        article.dataset.proceed = true;
     }
-
-    let button = document.createElement('button');
-    button.style.backgroundColor = localStorage.getItem(id) ? proceedColor : unproceedColor;
-    button.addEventListener('click', (evt) => {
-      download(id, button);
-    });
-    button.innerText = "click to save";
-
-    progressReceiver(button);
-
-    article.appendChild(button);
-    article.dataset.proceed = true;
-  }
 }
 
 function progressReceiver(btn) {
-  let totalReceived = 0;
-  window.addEventListener("message", (evt) => {
-    let {type: type} = evt.data;
+    let totalReceived = 0;
+    window.addEventListener("message", (evt) => {
+        let {type: type} = evt.data;
 
-    switch (type) {
-    case "progress":
-      let { amount: amount } = evt.data;
-      totalReceived += amount;
-      btn.innerText = `${totalReceived} bytes received`;
-      break;
-      
-    case "compress":
-      let { filename: filename, percent: percent } = evt.data;
-      btn.innerText = `compressing file: ${filename} (${percent}%)`;
-      break;
-      
-    }
-  });
+        switch (type) {
+            case "progress":
+                let { amount: amount } = evt.data;
+                totalReceived += amount;
+                btn.innerText = `${totalReceived} bytes received`;
+                break;
+
+            case "compress":
+                let { filename: filename, percent: percent } = evt.data;
+                btn.innerText = `compressing file: ${filename} (${percent}%)`;
+                break;
+
+        }
+    });
 }
 
 main();
